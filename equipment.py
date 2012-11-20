7a89c11691c1f0782be7f40d08863b959b601d0e
Matthew J. Brown <matthew_brown34@hotmail.com>
2012-11-20 16:02:12 -0500

    Functional Character creation with initial inventory.

---
+++ equipment.py
@@ -0,0 +1,45 @@
+"""
+Created on November 14, 2012
+
+@author Matthew
+"""
+#info for item addition========
+	# [item1 in list = Name, item2 = Description of item, item3 = amount of dice to roll for damage, item4 = sides of the dice to roll]
+def addItem(self, fileName, listName, itemName):
+		imported = getattr(__import__(fileName, fromlist=[listName]), listName)
+		x=0
+		for item in imported:
+			if itemName==imported[x][0]:
+				return imported[x]
+			else:
+			        x+=1
+
+meleeList= [
+	    ["Dagger", "A very small knife. Only suitable for emergency situations.", 1, 4], 
+	    ["Short Sword", "A small sword used to fight smaller foes.", 1, 6],
+	    ["Long Sword", "A decent sword used in combat.", 1, 8], 
+	    ["Staff", "A wooden staff engraved with the user's insignia.", 1, 6],
+	    ["Mace", "A large club used typically by Clerics.", 1, 8], 
+	    ["Warhammer", "A heavy, two-handed weapon capable of a large amount of damage", 2, 8]
+	   ] 
+#ranged weapons follow the same formula above, with the addition of a fifth item in the list used for the range of the weapon
+rangedList= [
+	     ["Short Bow", "A small bow used for hunting.", 1, 6, 350], 
+	     ["Long Bow", "A large bow used by the military for long-range combat.", 2,6, 500],
+	     ["Sling", "A small slingshot which hurls stones at your enemies.", 1, 6, 200],
+	     ["Throwing Knives", "Sharp knives that deal a good amount of damage.", 2, 4, 100]
+	    ]
+#armor addition is name, description, defense value, dexterity penalty
+armorList= [
+	    ["Mage's Robes", "Lightweight robes providing little protection.", 10, 0],
+	    ["Monk's Garb", "Heavier cloth providing more protection than standard robes.", 15, 0], 
+            ["Leather Armor", "Lightweight armor that provides decent protection.", 20, 0],
+	    ["Studded Armor", "Leather armor with studs to deflect sword slashes.", 25, 0], 
+            ["Hide Armor", "Armor made from the tough hides of animals.", 15, 1],
+	    ["Scale Armor", "Leather armor made from the hard scales of creatures.", 30, 2], 
+            ["Chainmail", "Linked metal rings that provide a lot of protection.", 35, 3],
+            ["Steel Armor", "Heavy armor forged to withstand a lot of damage.", 40, 4], 
+	    ["Plate Armor", "Iron plates forged and pieced together to allow superior protection.", 50, 5],
+	    ["Small Shield","A small shield used to increase defense.", 15, 2],
+	    ["Medium Shield", "A larger shield that provides a substantial boost to defense.", 25, 3],
+	   ]
--- generate.py
+++ generate.py
@@ -7,18 +7,21 @@ Created on November 11, 2012
 import diceRoll
 #import spells
 #import skills
-#import equipment
+import equipment
 #import items
 class Player:
 	def __init__(self):
-		self.name=''
-		self.race=''
-		self.job=''
+		self.name='None'
+		self.race='None'
+		self.job='None'
 		self.statNames=["STR","DEX","CON","INT","WIS","CHA"]
 		self.statVals=[]
 		self.hp=0
-		self.equip=[]
+		self.mp=0
+		self.weapon=[]
+		self.range=[]
 		self.equipment=[]
+		self.armor=[]
 		self.items=[]
 		self.spells=[]
 		self.skills=[]
@@ -27,10 +30,12 @@ class Player:
 		self.addName()
 		self.addRace()
 		self.setVals()
-		#self.raceBonus()
+		self.raceBonus()
 		self.setHP()
+		self.setMP()
+		self.displayInfo()
 		self.addJob()
-		#self.initialEquip()
+		self.initialEquip()
 		#self.initialInv()
 		#self.initialSpells()
 		#self.initialSkills()
@@ -41,12 +46,12 @@ class Player:
 	
 	def addName(self):
 		print "\nWhat will the character's name be?\n"
-		self.name=raw_input(">>")	
+		self.name=raw_input(">> ")	
 	def addRace(self):
 		choice = 0
 		while choice < 1 or choice > 5:
 			print "\nWhat is the new character's race?\n\n\t1) Human   2) Elven   3) Half-Elven   4) Dwarven   5) Halfling\n"
-			choice = input(">>")
+			choice = input(">> ")
 			if choice == 1:
 				self.race='Human'
 			elif choice == 2:
@@ -64,7 +69,7 @@ class Player:
 		choice=0
 		while choice < 1 or choice > 6:
 			print "\nWhat will the new character's class be?\n\n\t1) Fighter   2) Mage   3) Thief   4) Ranger  5) Cleric  6) Monk\n"
-			choice = input(">>")
+			choice = input(">> ")
 			if choice ==1:
 				self.job='Fighter'
 			elif choice ==2:
@@ -100,6 +105,27 @@ class Player:
 		elif self.statVals[2] >2:
 			dice=8
 		self.hp=die.roll(dice,6)
+	def setMP(self):
+		die=diceRoll.Die()
+		if self.statVals[4] >18:
+			dice=14
+		elif self.statVals[4] >16:
+			dice=13
+		elif self.statVals[4] >14:
+			dice=12
+		elif self.statVals[4] >12:
+			dice=11
+		elif self.statVals[4] >10:
+			dice=10
+		elif self.statVals[4] >8:
+			dice=9
+		elif self.statVals[4] >6:
+			dice=8
+		elif self.statVals[4] >4:
+			dice=7
+		elif self.statVals[4] >1:
+			dice=6
+		self.mp=die.roll(dice,6)
 	def setVals(self):
 		die=diceRoll.Die()
 		x=0
@@ -120,7 +146,7 @@ class Player:
 			for i in self.statNames and self.statVals:
 				print"\t", n, ")", self.statNames[n-1], ": ", self.statVals[n-1], "\n"
 				n+=1
-			choice = input("\n>>")
+			choice = input("\n>> ")
 			if choice == 1:
 				if self.statVals[choice-1] < 18:
 					self.statVals[choice-1] +=1
@@ -148,9 +174,58 @@ class Player:
 			else:
 				print"\nI am sorry, either that is not a valid choice or that stat is maxed out.  Please try again..."
 	def raceBonus(self):
-		print
+		#statVals[0]=STR [1]=DEX [2]=CON [3]=INT [4]=WIS [5]=CHA
+		#Human Elven Half-Elven Dwarven Halfling
+		if self.race == 'Elven':
+			self.statVals[1] +=2
+			self.statVals[2] -=1
+			self.statVals[3] +=1
+		elif self.race == 'Half-Elven':
+			self.statVals[1] +=2
+			self.statVals[2] -=1
+		elif self.race == 'Dwarven':
+			self.statVals[0]+=2
+			self.statVals[2] +=3
+			self.statVals[3] -=2
+			self.statVals[5] -=2
+		elif self.race == 'Halfling':
+			self.statVals[1] +=3
+			self.statVals[2] -=1
+			self.statVals[3] +=1
+			self.statVals[5] -=2
 	def initialEquip(self):
-		print
+		if self.job=='Fighter':
+			self.weapon.append(equipment.addItem(self,"equipment","meleeList","Long Sword"))
+			self.range.append(equipment.addItem(self,"equipment","rangedList","Short Bow"))
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Medium Shield"))
+			self.armor.append(equipment.addItem(self,"equipment","armorList", "Studded Armor"))
+		elif self.job=='Mage':
+			self.weapon.append(equipment.addItem(self,"equipment", "meleeList","Staff"))
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Mage's Robes"))
+		elif self.job=='Thief':
+			self.weapon.append(equipment.addItem(self,"equipment", "meleeList","Dagger"))
+			self.range.append(equipment.addItem(self,"equipment", "rangedList","Short Bow"))
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Leather Armor"))
+		elif self.job=='Ranger':
+			self.weapon.append(equipment.addItem(self,"equipment", "meleeList","Long Sword"))
+			self.range.append(equipment.addItem(self,"equipment", "rangedList","Long Bow"))
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Studded Armor"))
+		elif self.job=='Cleric':
+			self.weapon.append(equipment.addItem(self,"equipment", "meleeList","Mace"))
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Small Shield"))
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Scale Armor"))
+		elif self.job=='Monk':
+			self.armor.append(equipment.addItem(self,"equipment", "armorList","Monk's Garb"))
+	def displayEquip(self):
+			print "Your equipped melee weapon is:"
+			for x in self.weapon: 
+				print x
+			print "Your equipped ranged weapon is:"
+			for x in self.range: 
+				print x
+			print "Your equipped armor is:"
+			for x in self.armor: 
+				print x
 	def initialInv(self):
 		print
 	def initialSpells(self):
@@ -165,4 +240,5 @@ class Player:
 			print "\t", self.statNames[x], ":", self.statVals[x], "\n"
 			x+=1
 		print"Your hp is:", self.hp, " "
+		print"Your mp is:", self.mp, " "
 		
